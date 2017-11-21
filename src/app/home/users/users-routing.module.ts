ac0247abc551acb4f9ed5855e0cda9e7bb1b0982
Mubashir Baig <mubashirbaig@Mubashirs-MacBook-Pro.local>
2017-11-22 01:23:11 +0500

    updated user routes

--- src/app/home/users/users-routing.module.ts
+++ src/app/home/users/users-routing.module.ts
@@ -1,10 +1,16 @@
+import { UsersListComponent } from './users-list/users-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsersComponent } from './users.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-    { path: '', component: UsersComponent }
+    { path: '', component: UsersComponent, children: [
+        { path: '', component: UsersListComponent },
+        { path: 'new', component: UserComponent },
+        { path: ':id/edit', component: UserComponent }
+    ] }
 ]
 
 @NgModule({
--- src/app/home/users/users.module.ts
+++ src/app/home/users/users.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { PageHeaderModule } from './../../shared/modules';
+import { UserComponent } from './user/user.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
     declarations: [
-        UsersComponent
+        UsersComponent,
+        UserComponent,
+        UsersListComponent
     ],
     imports: [
         CommonModule,
