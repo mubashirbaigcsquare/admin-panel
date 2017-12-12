d127d73412bc36fc2d2010f373a09ec78e966d63
mubashirbaigcsquare <mubashir.baig@csquare.com>
2017-12-12 16:57:37 +0500

    deploy

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  isActive = false;
+  showMenu = '';
+  eventCalled() {
+      this.isActive = !this.isActive;
   }
-
+  addExpandClass(element: any) {
+      if (element === this.showMenu) {
+          this.showMenu = '0';
+      } else {
+          this.showMenu = element;
+      }
+  }
 }
